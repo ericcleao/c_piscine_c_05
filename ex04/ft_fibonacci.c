1f48c86cbf35c3fd94c89aea7adc554d43aab30a
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-12-06 23:16:43 -0300

    c_piscine_c_05 Add exercise 04
    - Exercise "ft_fibonacci"
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

---
+++ ex04/ft_fibonacci.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/05 16:42:18 by ecerquei          #+#    #+#             */
+/*   Updated: 2019/12/05 16:46:26 by ecerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	else if (index < 0)
+		return (-1);
+	else
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
