c0f6a61a756841879def4d3b64b0d5015f6c5dd1
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-12-06 23:06:57 -0300

    c_piscine_c_05 Add exercise 02
    - Exercise "ft_interative_power"
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

---
+++ ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/05 16:33:32 by ecerquei          #+#    #+#             */
+/*   Updated: 2019/12/05 16:33:52 by ecerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int i;
+	int result;
+
+	if (power < 0)
+		return (0);
+	result = 1;
+	i = 0;
+	while (i < power)
+	{
+		result *= nb;
+		i++;
+	}
+	return (result);
+}
