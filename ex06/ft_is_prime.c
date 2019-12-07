d5a9c6a341ad6797528cf922e0762ad39c7bb0a9
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-12-06 23:24:21 -0300

    c_piscine_c_05 Add exercise 06
    - Exercise "ft_is_prime"
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

---
+++ ex06/ft_is_prime.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_prime.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/05 17:36:00 by ecerquei          #+#    #+#             */
+/*   Updated: 2019/12/05 18:06:27 by ecerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_sqrt(int nb);
+
+int	ft_is_prime(int nb)
+{
+	int i;
+	int sqrt_nb;
+
+	if (nb <= 1)
+		return (0);
+	i = 2;
+	sqrt_nb = ft_sqrt(nb);
+	while (i < sqrt_nb + 1)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_sqrt(int nb)
+{
+	int aux;
+	int i;
+
+	aux = 0;
+	i = 0;
+	while (i * i <= nb && i * i >= aux)
+	{
+		if (i * i == nb)
+			return (i);
+		aux = i * i;
+		i++;
+	}
+	return (0);
+}
